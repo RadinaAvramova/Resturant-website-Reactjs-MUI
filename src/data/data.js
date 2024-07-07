efd02f74298b25fbf3250ee5bab7b9417ca369fe
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-07-07 16:12:47 +0300

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -8,43 +8,37 @@ import Gujrati from "../images/gujrati.jpeg";
 export const MenuList = [
   {
     name: "Dosa",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    description:,
     image: Dosa,
     price: 200,
   },
   {
     name: "Chola",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    description:,
     image: Chola,
     price: 250,
   },
   {
     name: "Idli Sambhar",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    description:,
     image: Idli,
     price: 300,
   },
   {
     name: "Masala Dosa",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    description:,
     image: MasalaDosa,
     price: 100,
   },
   {
     name: "Paneer",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    description:,
     image: Paneer,
     price: 400,
   },
   {
     name: "Gujrati",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    description,
     image: Gujrati,
     price: 500,
   },
